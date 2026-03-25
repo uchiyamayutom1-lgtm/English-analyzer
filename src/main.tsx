5543efb26492dfb801dfb5f78612477a317782dd
Uchiyama Yuto <uchiyama.yuto.m1@elms.hokudai.ac.jp>
2026-03-25 20:42:58 +0900

    install analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react'; // + これを追加
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics /> {/* ←ここに追加！ */}
   </StrictMode>,
 )
